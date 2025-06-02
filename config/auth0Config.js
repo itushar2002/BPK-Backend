e78c0c9041f0a0e8ed596bd821a0af5ba7dd906f
itushar2002 <itusharjha2002@gmail.com>
2025-06-02 20:32:05 +0530

    backendupdated

--- config/auth0Config.js
+++ config/auth0Config.js
@@ -1,9 +1,9 @@
-import {auth} from "express-oauth2-jwt-bearer"
+import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
-    audience : "http://localhost:8000",
-    issuerBaseURL :   "https://dev-0zoy7it3pf1rqnr4.us.auth0.com",
-    tokenSigningAlg : "RS256",
- })
+  audience: "https://api.bhopalpropertyking.com",  // <-- Update this to your new API Identifier
+  issuerBaseURL: "https://dev-0zoy7it3pf1rqnr4.us.auth0.com",
+  tokenSigningAlg: "RS256",
+});
 
- export default jwtCheck;
+export default jwtCheck;
